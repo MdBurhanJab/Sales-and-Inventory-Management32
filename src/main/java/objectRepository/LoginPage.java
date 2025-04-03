beab089a98c51f17b23380573bc85858dfb8c191
Sabir-98278 <sabirmustafa1997@gmail.com>
2025-04-03 15:05:49 +0530

    i have added login pom page

---
+++ src/main/java/objectRepository/LoginPage.java
@@ -0,0 +1,36 @@
+package objectRepository;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+	public LoginPage(WebDriver driver) {
+		PageFactory.initElements(driver,this);
+	}
+	
+	@FindBy(name ="user")
+	private WebElement userName;
+	
+	@FindBy(name = "password")
+	private WebElement password;
+	
+	@FindBy(name = "btnlogin")
+	private WebElement submitButton;
+
+	public WebElement getUserName() {
+		return userName;
+	}
+
+	public WebElement getPassword() {
+		return password;
+	}
+
+	public WebElement getSubmitButton() {
+		return submitButton;
+	}
+	
+	
+}
